3bd5a1df27505e37df40b9eed507130b7255e012
Adnan <adnann3077@gmail.com>
2023-08-15 14:36:01 +0530

    Completed

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const timerContainer=document.querySelector(".timer span")
-let time=60;
+let time=59;
 setInterval(()=>{
     time=time-1;
     timerContainer.innerHTML=`00:00:${time}s`
+    if (time===0) {
+        time=60
+        
+    }
 },1000)
